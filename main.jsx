ae487124d972ed3269f3caccdeb05d41a0c6bbd5
rprofico-stack <renato@profico.ch>
2025-07-30 22:08:28 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,6 @@
-import React from 'react'; import ReactDOM from 'react-dom'; import ProficoWebsite from './ProficoWebsite.jsx'; ReactDOM.render(<ProficoWebsite />, document.getElementById('root'));
+import './index.css';                   // ← Styling aktivieren
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProficoWebsite from './ProficoWebsite.jsx';
+
+ReactDOM.render(<ProficoWebsite />, document.getElementById('root'));

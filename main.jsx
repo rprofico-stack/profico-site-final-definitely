7e3a5fa610ef4a69d0679056bf228be21dd74b89
rprofico-stack <renato@profico.ch>
2025-08-03 18:58:41 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ProficoWebsite from './ProficoWebsite.jsx'
+import ProficoWebsiteEN from './ProficoWebsiteEN.jsx'
 import './index.css'
+
+const path = window.location.pathname
+
+const App = () => {
+  if (path === '/en') {
+    return <ProficoWebsiteEN />
+  } else {
+    return <ProficoWebsite />
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProficoWebsite />
+    <App />
   </React.StrictMode>
 )
